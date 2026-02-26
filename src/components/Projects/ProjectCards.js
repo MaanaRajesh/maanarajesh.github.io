4402db08a1b7d9bcce99833116825d8714413898
Maanasa Rajeshwer <149624533+MaanaRajesh@users.noreply.github.com>
2026-02-26 03:02:32 -0500

    Update ProjectCards.js

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -13,10 +13,16 @@ function ProjectCards(props) {
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-  <BsGithub /> &nbsp;
-  GitHub
-</Button>
+        {props.ghLink && (
+          <Button
+            variant="primary"
+            href={props.ghLink}
+            target="_blank"
+            rel="noreferrer"
+          >
+    <BsGithub /> &nbsp; GitHub
+  </Button>
+)}
 
 {props.demoLink && (
   <Button
