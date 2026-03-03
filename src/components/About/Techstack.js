22e4e8836de0bd1487750193cedcda1866b0bc31
Maanasa Rajeshwer <149624533+MaanaRajesh@users.noreply.github.com>
2026-03-03 15:03:28 -0500

    fixed Techstack.js & Toolstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 
-// SVGs (inline)
+// Inline-safe SVGs
 import { ReactComponent as CppIcon } from "../../Assets/TechIcons/C++.svg";
 import { ReactComponent as PythonIcon } from "../../Assets/TechIcons/Python.svg";
 import { ReactComponent as DockerIcon } from "../../Assets/TechIcons/Docker.svg";
 import { ReactComponent as GitIcon } from "../../Assets/TechIcons/Git.svg";
-import { ReactComponent as Ros2Icon } from "../../Assets/TechIcons/ros2.svg";
-import { ReactComponent as MujocoIcon } from "../../Assets/TechIcons/mujoco.svg";
+
+// SVGs that break SVGR -> import as URL + render as <img>
+import ros2Svg from "../../Assets/TechIcons/ros2.svg";
+import mujocoSvg from "../../Assets/TechIcons/mujoco.svg";
 
 // PNGs
 import pytorchPng from "../../Assets/TechIcons/pytorch.png";
@@ -20,22 +22,19 @@ import mlflowPng from "../../Assets/TechIcons/mlflow.png";
 
 function Techstack() {
   const items = [
-    // Core
     { label: "Python", node: <PythonIcon className="tech-icon-svg" aria-label="Python" /> },
     { label: "C++", node: <CppIcon className="tech-icon-svg" aria-label="C++" /> },
     { label: "PyTorch", node: <img src={pytorchPng} alt="PyTorch" className="tech-icon-img" /> },
-    { label: "ROS 2", node: <Ros2Icon className="tech-icon-svg" aria-label="ROS 2" /> },
+    { label: "ROS 2", node: <img src={ros2Svg} alt="ROS 2" className="tech-icon-img" /> },
 
-    // Perception / tooling
     { label: "OpenCV", node: <img src={opencvPng} alt="OpenCV" className="tech-icon-img" /> },
     { label: "NumPy", node: <img src={numpyPng} alt="NumPy" className="tech-icon-img" /> },
     { label: "Linux", node: <img src={linuxPng} alt="Linux" className="tech-icon-img" /> },
     { label: "Docker", node: <DockerIcon className="tech-icon-svg" aria-label="Docker" /> },
     { label: "Git", node: <GitIcon className="tech-icon-svg" aria-label="Git" /> },
 
-    // Sim / frameworks
     { label: "Isaac Sim", node: <img src={isaacPng} alt="Isaac Sim" className="tech-icon-img" /> },
-    { label: "MuJoCo", node: <MujocoIcon className="tech-icon-svg" aria-label="MuJoCo" /> },
+    { label: "MuJoCo", node: <img src={mujocoSvg} alt="MuJoCo" className="tech-icon-img" /> },
     { label: "Robosuite", node: <img src={robosuitePng} alt="Robosuite" className="tech-icon-img" /> },
     { label: "MLflow", node: <img src={mlflowPng} alt="MLflow" className="tech-icon-img" /> },
   ];
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 
-// SVGs (inline)
+// Inline-safe SVGs
 import { ReactComponent as VsCodeIcon } from "../../Assets/TechIcons/vscode.svg";
 import { ReactComponent as DockerIcon } from "../../Assets/TechIcons/Docker.svg";
 
@@ -15,52 +15,26 @@ import isaacPng from "../../Assets/TechIcons/Isaac.png";
 import rvizPng from "../../Assets/TechIcons/rviz.png";
 
 function Toolstack() {
+  const items = [
+    { label: "Ubuntu", node: <img src={ubuntuPng} alt="Ubuntu" className="tech-icon-img" /> },
+    { label: "VS Code", node: <VsCodeIcon className="tech-icon-svg" aria-label="VS Code" /> },
+    { label: "GitHub", node: <img src={githubPng} alt="GitHub" className="tech-icon-img" /> },
+    { label: "Docker", node: <DockerIcon className="tech-icon-svg" aria-label="Docker" /> },
+    { label: "Conda", node: <img src={condaPng} alt="Conda" className="tech-icon-img" /> },
+    { label: "SSH", node: <img src={sshPng} alt="SSH" className="tech-icon-img" /> },
+    { label: "Gazebo", node: <img src={gazeboPng} alt="Gazebo" className="tech-icon-img" /> },
+    { label: "Isaac Sim", node: <img src={isaacPng} alt="Isaac Sim" className="tech-icon-img" /> },
+    { label: "rviz", node: <img src={rvizPng} alt="rviz" className="tech-icon-img" /> },
+  ];
+
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={6} md={3} className="tech-icons">
-        <img src={ubuntuPng} alt="Ubuntu" className="tech-icon-img" />
-        <div className="tech-icons-text">Ubuntu</div>
-      </Col>
-
-      <Col xs={6} md={3} className="tech-icons">
-        <VsCodeIcon className="tech-icon-svg" aria-label="VS Code" />
-        <div className="tech-icons-text">VS Code</div>
-      </Col>
-
-      <Col xs={6} md={3} className="tech-icons">
-        <img src={githubPng} alt="GitHub" className="tech-icon-img" />
-        <div className="tech-icons-text">GitHub</div>
-      </Col>
-
-      <Col xs={6} md={3} className="tech-icons">
-        <DockerIcon className="tech-icon-svg" aria-label="Docker" />
-        <div className="tech-icons-text">Docker</div>
-      </Col>
-
-      <Col xs={6} md={3} className="tech-icons">
-        <img src={condaPng} alt="Conda" className="tech-icon-img" />
-        <div className="tech-icons-text">Conda</div>
-      </Col>
-
-      <Col xs={6} md={3} className="tech-icons">
-        <img src={sshPng} alt="SSH" className="tech-icon-img" />
-        <div className="tech-icons-text">SSH</div>
-      </Col>
-
-      <Col xs={6} md={3} className="tech-icons">
-        <img src={gazeboPng} alt="Gazebo" className="tech-icon-img" />
-        <div className="tech-icons-text">Gazebo</div>
-      </Col>
-
-      <Col xs={6} md={3} className="tech-icons">
-        <img src={isaacPng} alt="Isaac Sim" className="tech-icon-img" />
-        <div className="tech-icons-text">Isaac Sim</div>
-      </Col>
-
-      <Col xs={6} md={3} className="tech-icons">
-        <img src={rvizPng} alt="rviz" className="tech-icon-img" />
-        <div className="tech-icons-text">rviz</div>
-      </Col>
+      {items.map((item) => (
+        <Col key={item.label} xs={6} md={3} className="tech-icons">
+          {item.node}
+          <div className="tech-icons-text">{item.label}</div>
+        </Col>
+      ))}
     </Row>
   );
 }
