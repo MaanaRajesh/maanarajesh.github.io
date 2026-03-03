30a1b3a7aeefe0e09c2700abc8642f5cabd1a209
Maanasa Rajeshwer <149624533+MaanaRajesh@users.noreply.github.com>
2026-03-03 13:01:30 -0500

    Updated techstack and toolstack, deleted images and added necessary images

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,28 +1,31 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import macOs from "../../Assets/TechIcons/Apple MacOSX.svg";
-import chrome from "../../Assets/TechIcons/Google Chrome.svg";
-import vsCode from "../../Assets/TechIcons/vscode.svg";
-import intelliJ from "../../Assets/TechIcons/intellij-idea.svg";
+
+import { ReactComponent as MacIcon } from "../../assets/techicons/Apple MacOSX.svg";
+import { ReactComponent as ChromeIcon } from "../../assets/techicons/Google Chrome.svg";
+import { ReactComponent as VsCodeIcon } from "../../assets/techicons/vscode.svg";
+import { ReactComponent as IntelliJIcon } from "../../assets/techicons/intellij-idea.svg";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <img src={macOs} alt="macOs" className="tech-icon-images" />
-        <div className="tech-icons-text">Mac Os</div>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons ">
-        <img src={chrome} alt="Chrome" className="tech-icon-images" />
-        <div className="tech-icons-text">Google Chrome</div>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons ">
-        <img src={vsCode} alt="vsCode" className="tech-icon-images" />
-        <div className="tech-icons-text">Vs Code</div>
+      <Col xs={6} md={3} className="tech-icons">
+        <MacIcon className="tech-icon-svg" aria-label="macOS" />
+        <div className="tech-icons-text">macOS</div>
       </Col>
 
-      <Col xs={4} md={2} className="tech-icons ">
-        <img src={intelliJ} alt="go" className="tech-icon-images" />
+      <Col xs={6} md={3} className="tech-icons">
+        <ChromeIcon className="tech-icon-svg" aria-label="Chrome" />
+        <div className="tech-icons-text">Chrome</div>
+      </Col>
+
+      <Col xs={6} md={3} className="tech-icons">
+        <VsCodeIcon className="tech-icon-svg" aria-label="VS Code" />
+        <div className="tech-icons-text">VS Code</div>
+      </Col>
+
+      <Col xs={6} md={3} className="tech-icons">
+        <IntelliJIcon className="tech-icon-svg" aria-label="IntelliJ" />
         <div className="tech-icons-text">IntelliJ</div>
       </Col>
     </Row>
